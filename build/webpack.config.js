7a20867929d9cfcb0a5f168581447d37791a26e4
biggerHacker <1049025971@qq.com>
2018-04-29 19:08:30 +0800

    react-app-cli

---
+++ build/webpack.config.js
@@ -0,0 +1,93 @@
+const webpack = require('webpack')
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  entry: {
+    app: path.join(__dirname, '../src/app.js')
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/public/'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: [
+          path.join(__dirname, '../node_modules')
+        ]
+      },
+      {
+        oneOf: [
+          {
+            test: /\.(js|jsx)$/,
+            loader: 'babel-loader',
+            exclude: [
+              path.join(__dirname, '../node_modules')
+            ]
+          },
+          {
+            test: [/\.jpe?g$/, /\.png$/, /\.gif$/, /\.bmp$/],
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: '[name].[hash:8].[ext]'
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader'
+              }
+            ]
+          },
+          {
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, '../template/index.html')
+    })
+  ]
+}
+
+if (isDev) {
+  config.entry = {
+    app: [
+      'react-hot-loader/patch',
+      path.join(__dirname, '../src/app.js')
+    ]
+  }
+  config.devServer = {
+    host: '0.0.0.0',
+    port: '8888',
+    contentBase: path.join(__dirname, '../dist'),
+    hot: true,
+    overlay: {
+      errors: true
+    },
+    publicPath: '/public',
+    historyApiFallback: {
+      index: '/public/index.html'
+    }
+  }
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
+module.exports = config
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = () => <div>hello react</div>
+
+export default App
